4a4b2f4796d68ebad763c6631196a3ccbb139348
zohar <zoharor3@gmail.com>
2022-11-26 20:38:38 +0200

    Dijakstra shortest path

--- HW1.py
+++ HW1.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 from typing import List, Tuple
-from numpy.linalg import norm
+from scipy.spatial import distance
 
 from Plotter import Plotter
 from shapely.geometry.polygon import Polygon, LineString
@@ -27,8 +27,6 @@ def get_minkowsky_sum(original_shape: Polygon, r: float) -> Polygon:
         tmpObstaclePoints[cSpacetmpIndex*4 + 3] = tuple(map(lambda i, j: i + j, original_shape.exterior.coords[cSpacetmpIndex] , (-r, 0)))
 
     tmpObstaclePolygon = Polygon(tmpObstaclePoints)
-    # print(tmpObstaclePoints)
-    # print(tmpObstaclePolygon.convex_hull)
 
     return tmpObstaclePolygon.convex_hull
 
@@ -46,6 +44,7 @@ def check_crossed_polygon(obstacles: List[Polygon], cords1,cords2):
 
 
 
+
 # TODO
 def get_visibility_graph(obstacles: List[Polygon], source=None, dest=None) -> List[LineString]:
     """
@@ -56,23 +55,22 @@ def get_visibility_graph(obstacles: List[Polygon], source=None, dest=None) -> Li
     :return: A list of LineStrings holding the edges of the visibility graph
     """
     line_string_list = []
-    edge_list = []
+    node_list = []
+    if source != None and dest != None:
+        node_list.append(source)
+        node_list.append(dest)
     for obstacle in obstacles:
         for cords in obstacle.exterior.coords:
-            edge_list.append(cords)
+            node_list.append(cords)
+    distance_matrix = [[0 for x in range(len(node_list))] for y in range(len(node_list))]
+    for cords1_ind, cords2_ind in combinations(range(len(node_list)), 2):
+        if check_crossed_polygon(obstacles, node_list[cords1_ind], node_list[cords2_ind]) is False:
+            line_string_list.append(LineString([node_list[cords1_ind], node_list[cords2_ind]]))
+            distance_matrix[cords1_ind][cords2_ind] = distance.euclidean(node_list[cords1_ind], node_list[cords2_ind])
+            distance_matrix[cords2_ind][cords1_ind] = distance.euclidean(node_list[cords1_ind], node_list[cords2_ind])
 
-    for cords1, cords2 in combinations(edge_list, 2):
-        if check_crossed_polygon(obstacles,cords1,cords2) is False:
-            line_string_list.append(LineString([cords1,cords2]))
-    return line_string_list
 
-    # for object1, object2 in combinations(obstacles, 2):
-    #    for cords1 in object1.exterior.coords:
-    #        for cords2 in object2.exterior.coords:
-    #            if check_intersection(obstacles,cords1,cords2) is False:
-    #                line_string_list.append(LineString([cords1,cords2]))
-    # return line_string_list
-
+    return line_string_list, distance_matrix, node_list
 
 def is_valid_file(parser, arg):
     if not os.path.exists(arg):
@@ -85,38 +83,49 @@ def get_points_and_dist(line):
     source = tuple(map(float, source.split(',')))
     return source, dist
 
-# def find_shortest_path(lines: List[LineString], startingNode: tuple) -> tuple[LineString, float]:
-#     """
-#     find shortest path for given graph
-#     :param lines: A list of the graph edges
-#     :return: A LineStrings holding the edges of the shortest path and cost of that path
-#     """
-#     distances = []
-#     for line in lines:
-#         distances.append(norm(line))
-#
-#     visited = set()
-#     parentsMap = {}
-#     PriorityQueue = []
-#     nodeCosts = defaultdict(lambda: float('inf'))
-#     nodeCosts[startingNode] = 0
-#     heap.heappush(PriorityQueue, (0, startingNode))
-#
-#     while PriorityQueue:
-#         # go greedily by always extending the shorter cost nodes first
-#         _, node = heap.heappop(PriorityQueue)
-#         visited.add(node)
-#         print(PriorityQueue)
-#         for adjNode, weight in lines[node].items():
-#             if adjNode in visited:    continue
-#
-#             newCost = nodeCosts[node] + weight
-#             if nodeCosts[adjNode] > newCost:
-#                 parentsMap[adjNode] = node
-#                 nodeCosts[adjNode] = newCost
-#                 heap.heappush(PriorityQueue, (newCost, adjNode))
-#
-#     return parentsMap, nodeCosts
+def find_shortest_path(lines: List[LineString], distance_matrix : List[List], node_list : List) -> tuple[LineString, float]:
+    """
+    find shortest path for given graph
+    :param lines: A list of the graph edges
+    :return: A LineStrings holding the edges of the shortest path and cost of that path
+    """
+
+    visited = set()
+    parentsMap = {}
+    PriorityQueue = []
+    nodeCosts = defaultdict(lambda: float('inf'))
+    nodeCosts[0] = 0
+    heap.heappush(PriorityQueue, (0, 0))
+
+    while PriorityQueue:
+        # go greedily by always extending the shorter cost nodes first
+        node = heap.heappop(PriorityQueue)
+        if node[1] == 1:
+            break
+        visited.add(node[1])
+        for adjNode, adjNode_dist in enumerate(distance_matrix[node[1]]):
+            if adjNode_dist != 0:
+                if adjNode in visited:    continue
+
+                newCost = nodeCosts[node[1]] + adjNode_dist
+                if nodeCosts[adjNode] > newCost:
+                    parentsMap[adjNode] = node[1]
+                    nodeCosts[adjNode] = newCost
+                    heap.heappush(PriorityQueue, (newCost, adjNode))
+    child = 1
+    shortest_path = []
+    nodesTotCost = 0
+    shortest_path.append(node_list[0])
+    while parentsMap[child] != 0:
+        # shortest_path.append(LineString([node_list[0], node_list[parentsMap[child]]]))
+        shortest_path.append(node_list[parentsMap[child]])
+        child = parentsMap[child]
+        nodesTotCost += nodeCosts[child]
+    # shortest_path.append(LineString([node_list[child], node_list[1]]))
+    shortest_path.append(node_list[1])
+
+    # print("shortests path  is  ", shortest_path[1])
+    return shortest_path, nodesTotCost
 
 
 
@@ -154,8 +163,7 @@ if __name__ == '__main__':
     plotter1.show_graph()
 
     # step 2:
-
-    lines = get_visibility_graph(c_space_obstacles)
+    lines, distance_matrix, node_list =get_visibility_graph(c_space_obstacles)
     plotter2 = Plotter()
 
     plotter2.add_obstacles(workspace_obstacles)
@@ -165,25 +173,18 @@ if __name__ == '__main__':
 
     plotter2.show_graph()
 
-
-
-
     # step 3:
     with open(query, 'r') as f:
         dest = tuple(map(float, f.readline().split(',')))
 
-    # lines = get_visibility_graph(c_space_obstacles, source, dest)
-    lines = {"a" : (0.0, 0.5), "b" : (0.5, 0.0),"c" : (0.0, -0.5), "d":(-0.5, 0.0), "e":(1.0, 0.5),"f": (1.5, 0.0),"g": (1.0, -0.5), "h":(0.5, 1.5)} # only until visibility graph will be ready
-
+    lines, distance_matrix, node_list = get_visibility_graph(c_space_obstacles, source, dest)
     #TODO: fill in the next line
-    shortest_path, cost = find_shortest_path(lines, (0.5,0.0))
+    shortest_path, cost = find_shortest_path(lines, distance_matrix, node_list)
 
     plotter3 = Plotter()
     plotter3.add_robot(source, dist)
     plotter3.add_obstacles(workspace_obstacles)
     plotter3.add_robot(dest, dist)
-    # plotter3.add_visibility_graph(lines)
+    plotter3.add_visibility_graph(lines)
     plotter3.add_shorterst_path(list(shortest_path))
-
-
     plotter3.show_graph()
